9a6b8f29b2dde930939051117818a7df305e2328
Alexis Venegas <alexisvfilmstuff@protonmail.com>
2021-09-15 19:58:16 -0700

    giving render views of database

--- sql/schema.sql
+++ sql/schema.sql
@@ -29,3 +29,9 @@ CREATE TABLE employees (
 
 --giving views of the schema database input--
 
+SELECT employees.id, employees.first_name, employees.last_name, roles.title, roles.salary, departments.name AS 'department', CONCAT(manager.first_name, '', manager.last_name) AS 'manager'
+FROM employees
+LEFT JOIN roles ON employees.role_id = roles.id
+LEFT JOIN departments ON roles.department_id = departments.id
+LEFT JOIN employees manager ON manager.id = employees.manager_id
+
